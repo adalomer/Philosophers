ddf520a649029382f2a14ee7da0985f52eca5851
Omer Ali Adali <omadali@k1m23s01.42kocaeli.com.tr>
2025-05-10 18:35:13 +0300

    update

--- srcs/monitor.c
+++ srcs/monitor.c
@@ -6,7 +6,7 @@
 /*   By: omadali < omadali@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 00:03:23 by omadali           #+#    #+#             */
-/*   Updated: 2025/05/10 11:36:51 by omadali          ###   ########.fr       */
+/*   Updated: 2025/05/10 18:35:03 by omadali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static int	check_philo_death(t_data *data, int i)
 	pthread_mutex_lock(&data->philos[i].meal_mutex);
 	last_meal = data->philos[i].last_meal_time;
 	pthread_mutex_unlock(&data->philos[i].meal_mutex);
-	if ((get_time() - last_meal) > data->time_to_die)
+	if ((get_time() - last_meal) >= data->time_to_die)
 	{
 		pthread_mutex_lock(&data->write_mutex);
 		printf("%lld %d died\n", get_time() - data->start_time, data->philos[i].id + 1);
